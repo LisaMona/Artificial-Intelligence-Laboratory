92d0e509d5b5fd9730110b07cb9e327cb5be0eda
Mona Lisa <mnls4u@gmail.com>
2018-09-02 23:37:39 +0530

    Update a1.py

--- 29.08.2018/a1.py
+++ 29.08.2018/a1.py
@@ -12,50 +12,49 @@ training_data = [
     (array([1,1,1]), 1),
 ]
 
-w = random.rand(3)
-print(w)
 j=np.array([
     [0,0,1],
     [0,1,1],
     [1,0,1],
     [1,1,1],
 ])
+
 errors = []
 eta = 0.2
-n = 100
+w = random.rand(3)
+print(w)
+n = 40
 for i in range(n):
     x, expected = choice(training_data)
     result = dot(w, x)
     error = expected - unit_step(result)
     errors.append(error)
-    w += eta * error * x
+    w += eta * error * x    
+    
 print("\n")
 print("respective weights:",w) 
-print("\n")
-l = []
-for x, _ in training_data:
+print("\n") 
+l = []  
+for x, y in training_data:
     result = dot(x, w)
-    print("{}: -> {}".format(x[:2], unit_step(result)))
+    print("{} -> {}".format(x[:2], unit_step(result)))
     l.append(unit_step(result))
-    
+
 for d, sample in enumerate(j):
-    # Plot the negative samples
     if l[d] < 1:
         plt.scatter(sample[0], sample[1], s=120, marker='_', linewidths=2)
-    # Plot the positive samples
     else:
         plt.scatter(sample[0], sample[1], s=120, marker='+', linewidths=2)
-z = []
-k = []
+y = []
+x = []
 b = w[2]
 for x1 in range(-12,22):
-#for x in training_data:
-    x = float(x1/10)
-    k.append(x)
-    y = float((-(b/w[1])/(b/w[0]))*x - (b/w[1]))
-    #print(w,x,y)
-    z.append(y)
-plt.plot(k,z)
+    x2 = float(x1/10)
+    x.append(x2)
+    y2 = float((-(b/w[1])/(b/w[0]))*x2 - (b/w[1]))
+    y.append(y2)
+    
+plt.plot(x,y)
 plt.grid(True)
 plt.ylim(-1.5,2.5)
 plt.xlim(-1.5,2.5)
@@ -63,5 +62,3 @@ plt.xlabel("x-axis")
 plt.ylabel("y-axis")
 plt.title("OR Perceptron")
 plt.show()
-#plt.plot([-0.5,0.9],[0.9,-0.5])
-#plt.plot([0,1.5],[1.5,0])
